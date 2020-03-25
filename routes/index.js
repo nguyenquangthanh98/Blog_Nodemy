a5d788d4f822287bea5d7e3222e03f26d8a73846
chaudl113 <chaudl113@gmail.com>
2020-03-25 21:34:13 +0700

    update fe

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/signup', function(req, res, next) {
-//     res.render('signup');
-// });
-
+// /* GET home page. */
+router.get('/', function(req, res, next) {
+     res.render('index');
+ });
+ router.get('/about', (req, res) => {
+    console.log('Request for about page recieved');
+    res.render('about');
+  });
+  router.get('/new', (req, res) => {
+    console.log('Request for about page recieved');
+    res.render('new');
+  });
+  router.get('/contact', (req, res) => {
+    console.log('Request for about page recieved');
+    res.render('contact');
+  });
+  router.get('/single', (req, res) => {
+    console.log('Request for about page recieved');
+    res.render('single');
+  });
 module.exports = router;
