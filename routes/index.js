f3e276af5f427f4f895ddcb56bd070a50529939b
nguyenquangthanh.sl98 <nguyenquangthanh.sl98@gmail.com>
2020-03-31 21:40:07 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var userMidleware = require('../middleware/userMidleware');
 
 
 
 // /* GET home page. */
-router.get('/home', function(req, res, next) {
-     res.render('index');
- });
- router.get('/about', (req, res) => {
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+router.get('/about', userMidleware.checkXacThuc, (req, res) => {
     console.log('Request for about page recieved');
     res.render('about');
-  });
-  router.get('/new', (req, res) => {
+});
+router.get('/new', userMidleware.checkXacThuc, (req, res) => {
     console.log('Request for about page recieved');
     res.render('new');
-  });
-  router.get('/contact', (req, res) => {
+});
+router.get('/contact', userMidleware.checkXacThuc, (req, res) => {
     console.log('Request for about page recieved');
     res.render('contact');
-  });
-  router.get('/single', (req, res) => {
+});
+router.get('/single', userMidleware.checkXacThuc, (req, res) => {
     console.log('Request for about page recieved');
     res.render('single');
-  });
-  router.get('/menu', (req, res) => {
+});
+router.get('/admin', userMidleware.checkXacThuc, userMidleware.checkAdmin, (req, res) => {
     console.log('Request for about page recieved');
     res.render('MenuAd');
-  });
-  router.get('/quanlybaiviet', (req, res) => {
+});
+router.get('/admin/quanlybaiviet', (req, res) => {
     console.log('Request for about page recieved');
     res.render('post');
-  });
-  router.get('/signup', function(req, res, next) {
+});
+router.get('/signup', function(req, res, next) {
     res.render('signup');
 });
 router.get('/login', function(req, res, next) {
-  res.render('login');
+    res.render('login');
 });
 
 module.exports = router;
